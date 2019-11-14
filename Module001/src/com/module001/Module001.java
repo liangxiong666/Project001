d234273a054ce62a76d8fae5faa7d4b73b5a061b
liangxiong666 <405001832@qq.com>
2019-11-14 21:45:00 +0800

    add Module001

---
+++ Module001/src/com/module001/Module001.java
@@ -0,0 +1,26 @@
+package com.module001;
+
+/**
+ * @author YangLiang
+ * @create 2019-11-14-21:40
+ */
+public class Module001 {
+    String name;
+    String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
